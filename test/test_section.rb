557c95d5b948da780ea2db771ec64839147e895c
Nick Belzer <me@nickbelzer.me>
2024-05-16 11:52:39 +0200

    Add section with image test

--- test/test_section.rb
+++ test/test_section.rb
@@ -15,4 +15,18 @@ class TestSection < MinitestShopify::LiquidTest
     assert_selector "h2", text: "Hello world!"
     assert_selector "p", text: "A long paragraph of text goes here. It should be long enough to wrap to the next line."
   end
+
+  def test_renders_a_section_with_image_tag
+    MinitestShopify.configuration.theme_root = File.join(__dir__, "theme")
+
+    section_settings = {
+      "settings" => {
+        "title" => "Hello world!",
+        "image" => "files/image.jpg"
+      }
+    }
+    render template: "sections/image-with-title", variables: { "section" => section_settings }
+    assert_selector "h2", text: "Hello world!"
+    assert_element "img", "src": "files/image.jpg"
+  end
 end
