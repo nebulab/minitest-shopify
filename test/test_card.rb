97601c5d10bba5cde4a7f39bb28d266f5335e7de
Sean Denny <seand7565@gmail.com>
2024-09-04 15:00:28 -0400

    Allow symbol key values for liquid variables

--- test/test_card.rb
+++ test/test_card.rb
@@ -5,7 +5,7 @@ class TestCard < MinitestShopify::LiquidTest
   MinitestShopify.configuration.theme_root = File.join(__dir__, "theme")
 
   def test_renders_a_card
-    render template: "snippets/card", variables: { "comment" => default_comment }
+    render template: "snippets/card", variables: { comment: default_comment }
     assert_text "Hello world!"
     assert_text "John Doe"
   end
@@ -14,12 +14,12 @@ class TestCard < MinitestShopify::LiquidTest
 
   def default_comment
     {
-      "id" => 1,
-      "created_at" => "2023-07-20T19:31:35Z",
-      "content" => "Hello world!",
-      "author" => {
-        "id" => 1,
-        "name" => "John Doe"
+      id: 1,
+      created_at: "2023-07-20T19:31:35Z",
+      content: "Hello world!",
+      author: {
+        id: 1,
+        name: "John Doe"
       }
     }
   end
--- test/test_card_view.rb
+++ test/test_card_view.rb
@@ -5,7 +5,7 @@ class TestCardView < MinitestShopify::ViewTest
   MinitestShopify.configuration.theme_root = File.join(__dir__, "theme")
 
   def test_javascript_enabled_card
-    render template: "snippets/js-card", variables: { "comment" => default_comment }
+    render template: "snippets/js-card", variables: { comment: default_comment }
 
     within "#comment-1" do
       assert_text "Javascript is enabled"
@@ -17,8 +17,8 @@ class TestCardView < MinitestShopify::ViewTest
 
   def default_comment
     {
-      "id" => 1,
-      "content" => "Hello world!",
+      id: 1,
+      content: "Hello world!",
     }
   end
 end
